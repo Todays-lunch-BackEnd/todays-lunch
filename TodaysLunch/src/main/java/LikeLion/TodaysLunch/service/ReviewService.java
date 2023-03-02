c632caeb0ba8880e8adb3ce68614ab760188e5f1
hdsung98 <hdsung98@naver.com>
2023-03-03 03:42:41 +0900

    #32 Add: 작성자인지 여부 판단하는 서비스 메서드 생성 - 업데이트, 삭제 api에 필요

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/service/ReviewService.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/service/ReviewService.java
@@ -8,6 +8,7 @@ import LikeLion.TodaysLunch.repository.DataJpaRestaurantRepository;
 import LikeLion.TodaysLunch.repository.MenuRepository;
 import LikeLion.TodaysLunch.repository.ReviewRepository;
 import java.util.List;
+import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,86 +17,92 @@ import org.springframework.data.domain.Pageable;
 
 @Transactional
 public class ReviewService {
-  private final ReviewRepository reviewRepository;
-  private final DataJpaRestaurantRepository restaurantRepository;
+    private final ReviewRepository reviewRepository;
+    private final DataJpaRestaurantRepository restaurantRepository;
 
-  public ReviewService(ReviewRepository reviewRepository, DataJpaRestaurantRepository restaurantRepository) {
-    this.reviewRepository = reviewRepository;
-    this.restaurantRepository = restaurantRepository;
-  }
-  public Review create(Long restaurantId, ReviewDto reviewDto){
-    Restaurant restaurant = restaurantRepository.findById(restaurantId)
-        .orElseThrow(() -> new IllegalArgumentException("리뷰 생성 실패! 리뷰를 생성하기 위한 대상 맛집이 없습니다."));
-
-    Long count = restaurant.getReviewCount() + 1;
-    restaurant.setReviewCount(count);
-
-    Double rating = restaurant.getRating();
-    if(rating.equals(0.0)){
-      restaurant.setRating((double)reviewDto.getRating());
-    } else {
-      Pageable pageable = PageRequest.of(0, (int)reviewRepository.count());
-      List<Review> reviews= reviewRepository.findAllByRestaurant(restaurant, pageable).getContent();
-      Double sum = 0.0;
-      for(int i = 0; i < reviews.size(); i++){
-        sum += reviews.get(i).getRating();
-      }
-      sum += reviewDto.getRating();
-      restaurant.setRating((double)sum/count);
+    public ReviewService(ReviewRepository reviewRepository, DataJpaRestaurantRepository restaurantRepository) {
+        this.reviewRepository = reviewRepository;
+        this.restaurantRepository = restaurantRepository;
     }
 
-    restaurantRepository.save(restaurant);
+    public Review create(Long restaurantId, ReviewDto reviewDto) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new IllegalArgumentException("리뷰 생성 실패! 리뷰를 생성하기 위한 대상 맛집이 없습니다."));
 
-    Review review = reviewDto.toEntity();
-    review.setRestaurant(restaurant);
-    return reviewRepository.save(review);
-  }
+        Long count = restaurant.getReviewCount() + 1;
+        restaurant.setReviewCount(count);
 
-  public Page<Review> reviewsList(Long restaurantId, Pageable pageable){
-    Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
-    return reviewRepository.findAllByRestaurant(restaurant, pageable);
-  }
+        Double rating = restaurant.getRating();
+        if (rating.equals(0.0)) {
+            restaurant.setRating((double) reviewDto.getRating());
+        } else {
+            Pageable pageable = PageRequest.of(0, (int) reviewRepository.count());
+            List<Review> reviews = reviewRepository.findAllByRestaurant(restaurant, pageable).getContent();
+            Double sum = 0.0;
+            for (int i = 0; i < reviews.size(); i++) {
+                sum += reviews.get(i).getRating();
+            }
+            sum += reviewDto.getRating();
+            restaurant.setRating((double) sum / count);
+        }
 
-  public Review update(Long reviewId, Long restaurantId, ReviewDto reviewDto){
-    Review review = reviewRepository.findById(reviewId)
-        .orElseThrow(() -> new IllegalArgumentException("리뷰 수정 실패! 대상 리뷰가 없습니다."));
+        restaurantRepository.save(restaurant);
 
-    Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
-    Long count = restaurant.getReviewCount();
-    Pageable pageable = PageRequest.of(0, (int)reviewRepository.count());
-    List<Review> reviews= reviewRepository.findAllByRestaurant(restaurant, pageable).getContent();
-    Double sum = 0.0;
-    for(int i = 0; i < reviews.size()-1; i++){
-      sum += reviews.get(i).getRating();
+        Review review = reviewDto.toEntity();
+        review.setRestaurant(restaurant);
+        return reviewRepository.save(review);
     }
-    sum += reviewDto.getRating();
-    restaurant.setRating((double)sum/count);
 
-    restaurantRepository.save(restaurant);
-    review.update(reviewDto);
-
-    return reviewRepository.save(review);
-  }
-
-  public Review delete(Long reviewId, Long restaurantId){
-    Review review = reviewRepository.findById(reviewId)
-        .orElseThrow(() -> new IllegalArgumentException("리뷰 삭제 실패! 대상 리뷰가 없습니다."));
-
-    Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
-    Long count = restaurant.getReviewCount() - 1;
-    restaurant.setReviewCount(count);
-
-    Pageable pageable = PageRequest.of(0, (int)reviewRepository.count());
-    List<Review> reviews= reviewRepository.findAllByRestaurant(restaurant, pageable).getContent();
-    Double sum = 0.0;
-    for(int i = 0; i < reviews.size()-1; i++){
-      sum += reviews.get(i).getRating();
+    public Page<Review> reviewsList(Long restaurantId, Pageable pageable) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
+        return reviewRepository.findAllByRestaurant(restaurant, pageable);
     }
-    restaurant.setRating((double)sum/count);
 
-    restaurantRepository.save(restaurant);
+    public Review update(Long reviewId, Long restaurantId, ReviewDto reviewDto) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new IllegalArgumentException("리뷰 수정 실패! 대상 리뷰가 없습니다."));
 
-    reviewRepository.delete(review);
-    return review;
-  }
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
+        Long count = restaurant.getReviewCount();
+        Pageable pageable = PageRequest.of(0, (int) reviewRepository.count());
+        List<Review> reviews = reviewRepository.findAllByRestaurant(restaurant, pageable).getContent();
+        Double sum = 0.0;
+        for (int i = 0; i < reviews.size() - 1; i++) {
+            sum += reviews.get(i).getRating();
+        }
+        sum += reviewDto.getRating();
+        restaurant.setRating((double) sum / count);
+
+        restaurantRepository.save(restaurant);
+        review.update(reviewDto);
+
+        return reviewRepository.save(review);
+    }
+
+    public Review delete(Long reviewId, Long restaurantId) {
+        Review review = reviewRepository.findById(reviewId)
+                .orElseThrow(() -> new IllegalArgumentException("리뷰 삭제 실패! 대상 리뷰가 없습니다."));
+
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
+        Long count = restaurant.getReviewCount() - 1;
+        restaurant.setReviewCount(count);
+
+        Pageable pageable = PageRequest.of(0, (int) reviewRepository.count());
+        List<Review> reviews = reviewRepository.findAllByRestaurant(restaurant, pageable).getContent();
+        Double sum = 0.0;
+        for (int i = 0; i < reviews.size() - 1; i++) {
+            sum += reviews.get(i).getRating();
+        }
+        restaurant.setRating((double) sum / count);
+
+        restaurantRepository.save(restaurant);
+
+        reviewRepository.delete(review);
+        return review;
+    }
+
+    public boolean isReviewOwner(Long reviewId, Long restaurantId, Long memberId) {
+        Optional<Review> reviewOpt = reviewRepository.findByRestaurantIdAndId(restaurantId, reviewId);
+        return reviewOpt.isPresent() && reviewOpt.get().getMember().getId().equals(memberId);
+    }
 }
