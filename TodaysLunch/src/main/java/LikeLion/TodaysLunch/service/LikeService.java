4b18130e2cfce17433b8bec4f4491e2bafe5393c
hdsung98 <hdsung98@naver.com>
2023-03-02 23:21:15 +0900

    #30 Add: 찜 API 구현(메뉴,맛집)
    1) 멤버와 찜대상의 존재여부를 검증(현재 예외 catch해주는 곳 부재..수정예정)
    2) 멤버 도메인 내의 찜 리스트를 조회해서 해당 메뉴에 대한 찜이 존재하는지 체크
    3) 존재하는 경우 아무조치 없이 리턴, 존재하지 않는 경우 새로운 like객체 생성후 저장

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/service/LikeService.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/service/LikeService.java
@@ -3,11 +3,64 @@ package LikeLion.TodaysLunch.service;
 import LikeLion.TodaysLunch.domain.Member;
 import LikeLion.TodaysLunch.domain.Menu;
 import LikeLion.TodaysLunch.domain.Restaurant;
+import LikeLion.TodaysLunch.domain.relation.MenuLike;
+import LikeLion.TodaysLunch.domain.relation.RestaurantLike;
+import LikeLion.TodaysLunch.repository.MemberRepository;
+import LikeLion.TodaysLunch.repository.MenuRepository;
+import LikeLion.TodaysLunch.repository.RestaurantRepository;
+import LikeLion.TodaysLunch.service.exception.MemberNotFoundException;
+import LikeLion.TodaysLunch.service.exception.MenuNotFoundException;
+import LikeLion.TodaysLunch.service.exception.RestaurantNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class LikeService {
-    public void likeRestaurant(Member member, Restaurant restaurant) {
+    private final MemberRepository memberRepository;
+    private final MenuRepository menuRepository;
+
+    private final RestaurantRepository restaurantRepository;
+
+    @Autowired
+    public LikeService(MemberRepository memberRepository, MenuRepository menuRepository,
+                       RestaurantRepository restaurantRepository) {
+        this.memberRepository = memberRepository;
+        this.menuRepository = menuRepository;
+        this.restaurantRepository = restaurantRepository;
     }
 
-    public void likeMenu(Member member, Menu menu) {
+    @Transactional
+    public void createRestaurantLike(Long memberId, Long restaurantId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
+        Restaurant restaurant = restaurantRepository.findById(restaurantId)
+                .orElseThrow(() -> new RestaurantNotFoundException(restaurantId));
+
+        if (member.getRestaurantLikes()
+                .stream()
+                .anyMatch(like -> like.getRestaurant().getId().equals(restaurantId))) {
+            return;
+        }
+        member.addRestaurantLike(new RestaurantLike(member, restaurant));
+        memberRepository.save(member);
     }
+
+    @Transactional
+    public void createMenuLike(Long memberId, Long menuId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
+        Menu menu = menuRepository.findById(menuId)
+                .orElseThrow(() -> new MenuNotFoundException(menuId));
+
+        if (member.getMenuLikes()
+                .stream()
+                .anyMatch(like -> like.getMenu().getId().equals(menuId))) {
+            return;
+        }
+        member.addMenuLike(new MenuLike(member, menu));
+        menuRepository.save(menu);
+    }
+
+
 }
