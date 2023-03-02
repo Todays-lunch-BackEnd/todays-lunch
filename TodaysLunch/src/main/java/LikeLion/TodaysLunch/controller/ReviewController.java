6dd9b43a6c987a9e79abeaab784bc8ec7925d1e6
hdsung98 <hdsung98@naver.com>
2023-03-03 03:52:05 +0900

    #32 Add: preauthorize 인터셉터 추가 - 삭제, 변경 시 작성자인지 선 검증

--- TodaysLunch/src/main/java/LikeLion/TodaysLunch/controller/ReviewController.java
+++ TodaysLunch/src/main/java/LikeLion/TodaysLunch/controller/ReviewController.java
@@ -4,12 +4,12 @@ import LikeLion.TodaysLunch.domain.Review;
 import LikeLion.TodaysLunch.dto.ReviewDto;
 import LikeLion.TodaysLunch.service.ReviewService;
 import java.util.HashMap;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,41 +20,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReviewController {
-  private final ReviewService reviewService;
-  @Autowired
-  public ReviewController(ReviewService reviewService) {
-    this.reviewService = reviewService;
-  }
-  @PostMapping("/restaurants/{restaurantId}/reviews")
-  public ResponseEntity<Review> createReview(@RequestBody ReviewDto reviewDto, @PathVariable Long restaurantId){
-    if(reviewDto.getReviewContent() == null || reviewDto.getRating() == null){
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Review());
+    private final ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
     }
-    Review review = reviewService.create(restaurantId, reviewDto);
-    return ResponseEntity.status(HttpStatus.OK).body(review);
-  }
 
-  @GetMapping("/restaurants/{restaurantId}/reviews")
-  public ResponseEntity<HashMap<String, Object>> allReviewList(@PathVariable Long restaurantId, Pageable pageable){
-    Page<Review> reviews = reviewService.reviewsList(restaurantId, pageable);
-    HashMap<String, Object> responseMap = new HashMap<>();
-    responseMap.put("data", reviews.getContent());
-    responseMap.put("totalPages", reviews.getTotalPages());
-    return ResponseEntity.status(HttpStatus.OK).body(responseMap);
-  }
+    @PostMapping("/restaurants/{restaurantId}/reviews")
+    public ResponseEntity<Review> createReview(@RequestBody ReviewDto reviewDto, @PathVariable Long restaurantId) {
+        if (reviewDto.getReviewContent() == null || reviewDto.getRating() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Review());
+        }
+        Review review = reviewService.create(restaurantId, reviewDto);
+        return ResponseEntity.status(HttpStatus.OK).body(review);
+    }
 
-  @PatchMapping("/restaurants/{restaurantId}/reviews/{reviewId}")
-  public  ResponseEntity<Review> updateReview(@PathVariable Long reviewId, @PathVariable Long restaurantId,
-      @RequestBody ReviewDto dto){
-    Review updatedReview = reviewService.update(reviewId, restaurantId, dto);
-    return ResponseEntity.status(HttpStatus.OK).body(updatedReview);
-  }
+    @GetMapping("/restaurants/{restaurantId}/reviews")
+    public ResponseEntity<HashMap<String, Object>> allReviewList(@PathVariable Long restaurantId, Pageable pageable) {
+        Page<Review> reviews = reviewService.reviewsList(restaurantId, pageable);
+        HashMap<String, Object> responseMap = new HashMap<>();
+        responseMap.put("data", reviews.getContent());
+        responseMap.put("totalPages", reviews.getTotalPages());
+        return ResponseEntity.status(HttpStatus.OK).body(responseMap);
+    }
 
-  @DeleteMapping("/restaurants/{restaurantId}/reviews/{reviewId}")
-  public ResponseEntity<Review> delete(@PathVariable Long reviewId, @PathVariable Long restaurantId){
-    Review deletedReview = reviewService.delete(reviewId, restaurantId);
-    return ResponseEntity.status(HttpStatus.OK).body(deletedReview);
-  }
+    @PatchMapping("/restaurants/{restaurantId}/reviews/{reviewId}")
+    @PreAuthorize("@reviewService.isReviewOwner(#reviewId, #restaurantId, authentication.principal.id)")
+    public ResponseEntity<Review> updateReview(@PathVariable Long reviewId, @PathVariable Long restaurantId,
+                                               @RequestBody ReviewDto dto) {
+        Review updatedReview = reviewService.update(reviewId, restaurantId, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedReview);
+    }
+
+    @DeleteMapping("/restaurants/{restaurantId}/reviews/{reviewId}")
+    @PreAuthorize("@reviewService.isReviewOwner(#reviewId, #restaurantId, authentication.principal.id)")
+    public ResponseEntity<Review> delete(@PathVariable Long reviewId, @PathVariable Long restaurantId) {
+        Review deletedReview = reviewService.delete(reviewId, restaurantId);
+        return ResponseEntity.status(HttpStatus.OK).body(deletedReview);
+    }
 
 
 }
